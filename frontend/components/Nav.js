92fa22ac4ee5718c9b80ae523766a18169784ae5
Vikas Dwivedi <vikas.nice@gmail.com>
2020-06-09 21:08:12 +0530

    Showing total cart items with My Cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 import { TOGGLE_CART_MUTATION } from './Cart';
 import { Query, Mutation } from 'react-apollo';
 
@@ -34,7 +35,14 @@ const Nav = () => (
                 <Mutation mutation={TOGGLE_CART_MUTATION}>
                   {
                     (toggleCart) => (
-                      <button onClick={toggleCart}>My Cart</button>
+                      <button onClick={toggleCart}>My Cart
+                        <CartCount count={
+                          me.cart.reduce((tally, cartItem) => {
+                            return (tally + cartItem.quantity)
+                          }, 0)
+
+                        } />
+                      </button>
                     )
                   }
                 </Mutation>
